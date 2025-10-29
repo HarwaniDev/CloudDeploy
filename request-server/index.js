ef83c7a4fb75413294ff9195ae58499ecf184eed
Dev <devharwani.work@gmail.com>
2025-10-29 15:18:57 +0000

    add support for custom domains

--- request-server/index.js
+++ request-server/index.js
@@ -1,22 +1,30 @@
 const express = require('express')
 const httpProxy = require('http-proxy')
+const { Pool } = require('pg');
+require("dotenv").config();
 
 const app = express()
 const PORT = 8000
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
 
 const BASE_PATH = 'https://storage.googleapis.com/cloudeploy_assets/__outputs'
 
 const proxy = httpProxy.createProxy()
-
-app.use((req, res) => {
+console.log(process.env.DATABASE_URL);
+app.use(async (req, res) => {
     const hostname = req.hostname;
     const subdomain = hostname.split('.')[0];
-
-    // Custom Domain - DB Query
-
-    const resolvesTo = `${BASE_PATH}/${subdomain}`
-
-    return proxy.web(req, res, { target: resolvesTo, changeOrigin: true })
+    const client = await pool.connect();
+    try {
+        const { rows } = await client.query('SELECT id FROM "Project" WHERE subdomain = $1', [subdomain]);
+        const project = rows[0]?.id || subdomain;
+        const resolvesTo = `${BASE_PATH}/${project}`;
+        return proxy.web(req, res, { target: resolvesTo, changeOrigin: true })
+    } finally {
+        client.release();
+    }
 
 })
 
